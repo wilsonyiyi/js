ed6c7d2c4c65f31f012a062806e2818fa04a0d47
wilson <wilson.wang@ucloud.cn>
2020-08-11 08:49:01 +0800

    📖 learn api

--- share/formily/demo-learn-api/.umirc.ts
+++ share/formily/demo-learn-api/.umirc.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       routes: [
         {
           path: '/basic/schema',
-          name: 'schema',
+          name: '描述方式',
           component: './basic/schema',
         },
       ],
